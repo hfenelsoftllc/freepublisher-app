c74c68197d0c6a875e82e962a8a0e256d6514d6a
Fenel Hyacinthe <hfenelsoftllc@outlook.com>
2021-09-11 01:17:59 -0400

    Implement the application domain POJO

---
+++ src/main/java/com/hfenelsoftllc/freepublisherapp/FreepublisherAppApplication.java
@@ -0,0 +1,13 @@
+package com.hfenelsoftllc.freepublisherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreepublisherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreepublisherAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hfenelsoftllc/freepublisherapp/FreepublisherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hfenelsoftllc.freepublisherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreepublisherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
